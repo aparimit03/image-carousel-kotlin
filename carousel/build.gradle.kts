b9e97379fda9a05283330b3317b790281bcf04c9
Aparimit <aparimit2003@gmail.com>
2025-04-05 22:52:43 +0530

    artifacts issues fixed

--- carousel/build.gradle.kts
+++ carousel/build.gradle.kts
@@ -1,23 +1,39 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.library")
     alias(libs.plugins.kotlin.android)
     `maven-publish`
 }
 
 group = "com.github.aparimit03"
-version = "1.1.0"
+version = "1.2.0"
+
+afterEvaluate {
+    tasks.register("androidSourcesJar", Jar::class) {
+        archiveClassifier.set("sources")
+        from(android.sourceSets["main"].java.srcDirs)
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.aparimit03"
+                artifactId = "carousel"
+                version = "1.2.0"
+
+                artifact(tasks["androidSourcesJar"])
+            }
+        }
+    }
+}
 
 android {
     namespace = "com.example.carousel"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.carousel"
         minSdk = 22
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
