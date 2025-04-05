039d5e089a3711c6cc77bbf5e68011c7326436b0
Aparimit <aparimit2003@gmail.com>
2025-04-06 00:09:55 +0530

    v1.7.0

--- carousel/build.gradle.kts
+++ carousel/build.gradle.kts
@@ -1,11 +1,20 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.TaskProvider
+
 plugins {
     id("com.android.library")
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+    `maven-publish`
 }
 
 group = "com.github.aparimit03"
-version = "1.6.0"
+version = "1.7.0"
+
+val androidSourcesJar: TaskProvider<Jar> = tasks.register("androidSourcesJar", Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].java.srcDirs)
+}
 
 android {
     namespace = "com.example.carousel"
@@ -38,23 +47,20 @@ android {
     }
 }
 
-val androidSourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(android.sourceSets["main"].java.srcDirs)
-}
-
 afterEvaluate {
+    tasks.matching { it.name == "generateMetadataFileForReleasePublication" }.configureEach {
+        dependsOn(androidSourcesJar)
+    }
+
     publishing {
         publications {
             create<MavenPublication>("release") {
                 from(components["release"])
                 groupId = "com.github.aparimit03"
                 artifactId = "carousel"
-                version = "1.6.0"
+                version = "1.7.0"
 
-                artifact(androidSourcesJar.get()) {
-                    builtBy(androidSourcesJar)
-                }
+                artifact(androidSourcesJar.get())
             }
         }
     }
@@ -67,7 +73,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     api(libs.picasso)
     implementation(libs.glide)
     implementation(libs.androidx.multidex)
